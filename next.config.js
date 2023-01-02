852e846208a8868a3cb278bf3cc255cf7380d3a8
MoYuM <hanbaochang12138@gmail.com>
2023-01-02 22:34:57 +0800

    reset config

--- next.config.js
+++ next.config.js
@@ -19,7 +19,6 @@ const nextConfig = withMDX({
   reactStrictMode: true,
   swcMinify: true,
   pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
-  assetPrefix: './',
 })
 
 module.exports = nextConfig
